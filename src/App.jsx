83a5485ba8e8fabc64fc237c65761f9c396c2a60
Sebastian Benjamin Fuentes Toledo <sebastian.fuentest@sansano.usm.cl>
2024-06-02 17:45:05 -0400

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,55 @@
+import { useState } from "react"
+import "./styles.css"
+
+export default function App () {
+    const [newItem, setNewItem]= useState("")
+    const [todos, setTodos] = useState([])
+
+    function handleSubmit(e) {
+      e.preventDefault()
+
+      setTodos( (currentTodos) => {
+        return [
+          ...currentTodos,
+          {id: crypto.randomUUID(), title: newItem, completed: false},
+        ]
+      })
+
+      setNewItem("")
+    }
+
+    console.log(todos)
+    return (
+      <>
+        <form onSubmit={handleSubmit} className="new-item-form">
+          <div className="form-row">
+            <label htmlFor="item">New Item</label>
+            <input
+              value={newItem}
+              onChange={e => setNewItem(e.target.value)}
+              type="text" id="item"
+            />
+          </div>
+          <button className="btn">Add</button>
+        </form>
+
+        <h1 className="header">Todo List</h1>
+        <ul className="list">
+          {todos.map( todo => {
+            return <li key={todo.id}>
+            <label>
+              <input
+              type="checkbox" 
+              checked={todo.completed}
+              onChange={e => toggleTodo(todo.id, e.target.checked)}
+              />
+              {todo.title}
+            </label>
+            <button className="btn btn-danger">Delete</button>
+          </li>
+          })}
+          
+        </ul>
+      </>
+    )
+}
